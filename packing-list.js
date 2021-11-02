9b267166b8d9916bbb9b7f34fb3077febf033d95
Namrata Thapa <>
2021-11-02 13:08:10 -0400

    packing list exercise

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,15 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// for loop implementation
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
-packingList[3];
+
+// while loop implementation
+console.log("Kitchen stuff to pack:");
+let j = 0;
+while (j < packingList.length) {
+  console.log(packingList[j]);
+  j++;
+}
